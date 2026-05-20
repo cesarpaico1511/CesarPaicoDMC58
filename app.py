1c287b45c41c442a77b15684de6defa9f31b539e
Cesar Paico <cesarpaico@gmail.com>
2026-05-20 14:07:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ st.title("Mi primera aplicación en Pythom")  #Titulo
 st.sidebar.title("Parámetros")  #Barra lateral izquierda
 st.write("Elaborado por: Julio Cesar Paico Jaime") #write es el equivalente a print
 
-sesion = st.selectbox("Seleccione una sesión", ["Sesión 1", "Sesión 2", "Sesión 3", "Sesión 4"])
+sesion = st.sidebar.selectbox("Seleccione una sesión", ["Sesión 1", "Sesión 2", "Sesión 3", "Sesión 4"])

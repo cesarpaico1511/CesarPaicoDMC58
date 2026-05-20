d5153af0d23cc038b4047460a19ca32a974ed384
Cesar Paico <cesarpaico@gmail.com>
2026-05-20 14:13:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,31 @@ st.title("Mi primera aplicación en Pythom")  #Titulo
 st.sidebar.title("Parámetros")  #Barra lateral izquierda
 st.write("Elaborado por: Julio Cesar Paico Jaime") #write es el equivalente a print
 
+#st hace referencia a la libreria de streamlit, sidebar coloca a la izquierza, y selectbos, genera un combo con una lista de 4 opciones
 sesion = st.sidebar.selectbox("Seleccione una sesión", ["Sesión 1", "Sesión 2", "Sesión 3", "Sesión 4"])
+
+#estructuras de control
+
+if sesión == "Sesión 1":
+  st.write("Bienvenido a la sesión 1")
+else sesión == "Sesión 2":
+  st.write("Bienvenido a la sesión 2")
+else sesión == "Sesión 3":
+  st.write("Bienvenido a la sesión 3")
+else 
+  st.write("Bienvenido a la sesión4")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  

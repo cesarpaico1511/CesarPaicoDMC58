146cb91a667d7de6c9969b00be060ff9c83a2be6
Cesar Paico <cesarpaico@gmail.com>
2026-05-20 13:54:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title("Mi primera aplicación en Pythom")
+st.title("Mi primera aplicación en Pythom")  #Titulo
 st.sidebar.title("Parámetros")  #Barra lateral izquierda
-st.write("Elaborado por: Julio Cesar Paico Jaime")
+st.write("Elaborado por: Julio Cesar Paico Jaime") #write es el equivalente a print

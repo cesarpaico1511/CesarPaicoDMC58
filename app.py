ab4f6ff3625a92fb0aef41fd6d7c9de9b3cdbb28
Cesar Paico <cesarpaico@gmail.com>
2026-05-20 13:50:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import streamlit as st
 
 st.title("Mi primera aplicación en Pythom")
-
-st.sidebar.title("Parámetros")
-
+st.sidebar.title("Parámetros")  #Barra lateral izquierda
 st.write("Elaborado por: Julio Cesar Paico Jaime")
